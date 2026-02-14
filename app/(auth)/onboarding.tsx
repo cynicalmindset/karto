a86e0683a19f206a9c7ea419b36de5fb32e4200e
Bablu BLAST <12345678+yourusername@users.noreply.github.com>
2026-02-14 23:26:01 +0530

    onboarding screen

--- app/(auth)/onboarding.tsx
+++ app/(auth)/onboarding.tsx
@@ -1,28 +1,47 @@
 import ScreenWrapper from "@/components/ScreenWrapper";
+import { theme } from "@/theme";
+import { useRouter } from "expo-router";
 import React from "react";
-import { Image, StyleSheet, View, Text } from "react-native";
+import { Image, StyleSheet, View, Text, TouchableOpacity } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const Onboarding = () => {
-
+    const router = useRouter();
   const insets = useSafeAreaInsets();
 
   return (
     <ScreenWrapper scroll={false}>
       <View style={styles.container}>
 
-        <Image
-          source={require("../../assets/img/logo.png")}
-          style={[styles.logo, { top: insets.top + 10 }]}
-        />
+  
+  <View style={styles.topStack}>
+    <Image
+      source={require("../../assets/img/logo.png")}
+      style={[styles.logo, { marginTop: insets.top + 10 }]}
+    />
+  </View>
 
-        <View style={styles.centerStack}>
-          <Text style={styles.title}>
-            Create your Karto and never lose track of your cart.
-          </Text>
-        </View>
 
-      </View>
+  <View style={styles.centerStack}>
+    <Text style={styles.title}>
+      Create your{"\n"}Karto and{"\n"}never lose track{"\n"}of your cart.
+    </Text>
+  </View>
+
+ 
+  <View style={styles.bottomStack}>
+
+  <TouchableOpacity onPress={()=>{router.replace("/(auth)/login")}} style={styles.button}>
+  <Text style={styles.buttonText}>Login</Text>
+</TouchableOpacity>
+
+
+<TouchableOpacity onPress={()=>{router.replace("/(auth)/register")}} style={styles.button}>
+  <Text style={styles.buttonText}>Register</Text>
+</TouchableOpacity>
+</View>
+
+</View>
     </ScreenWrapper>
   );
 };
@@ -31,29 +50,55 @@ export default Onboarding;
 
 const styles = StyleSheet.create({
 
-  container: {
-    flex: 1,
-  },
+  
+container: {
+  flex: 1,
+},
 
-  logo: {
-    width: 55,
-    height: 55,
-    resizeMode: "contain",
-    position: "absolute",
-    left: 20,
-  },
+topStack: {
+  paddingLeft: 10,
+},
 
-  centerStack: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+centerStack: {
+  flex: 1,
+  justifyContent: "center",
+  alignItems: "flex-start",
+  paddingHorizontal: 10,
+  
+},
+
+bottomStack: {
+  //paddingHorizontal: 10,
+  paddingBottom: 10,
+  gap: 16,
+},
+
+
+logo: {
+  width: 55,
+  height: 55,
+  resizeMode: "contain",
+},
+
+title: {
+  fontSize: 28,
+  fontWeight: "700",
+  textAlign: "left",
+},
+ button: {
+    backgroundColor: theme.colors.secondary,
+    paddingVertical: 18,
     paddingHorizontal: 20,
+    borderRadius: 10,
+    alignItems: "center",
+    //marginTop:10
   },
 
-  title: {
-    fontSize: 20,
+  buttonText: {
+    color: "white",
+    fontSize: 16,
     fontWeight: "600",
-    textAlign: "center",
   },
+
 
 });
