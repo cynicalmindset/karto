39a361776bc862036f3beee2eb15939bbaa29f19
cynicalmindset <yashraj.02594656@gmail.com>
2026-02-15 20:36:39 +0530

    bottomsheet

--- app/(main)/profile.tsx
+++ app/(main)/profile.tsx
@@ -1,7 +1,9 @@
-import ProfileBottomSheet from "@/components/profilesheet";
+/*eslint-disable*/
+//import ProfileBottomSheet from "@/components/profilesheet";
+import BottomSheet , {BottomSheetView} from '@gorhom/bottom-sheet'
 import ScreenWrapper from "@/components/ScreenWrapper";
 import { useRouter } from "expo-router";
-import React, { useState } from "react";
+import React, { useMemo, useRef, useState } from "react";
 import {
     Image,
     ScrollView,
@@ -11,9 +13,18 @@ import {
     View,
 } from "react-native";
 import Header from "../../components/header";
+import { theme } from '@/theme';
+import { TextInput } from 'react-native-gesture-handler';
 //import { ScrollView } from 'react-native-reanimated/lib/typescript/Animated'
 
 const profile = () => {
+    const [name, setname] = useState("");
+    const [bio,setbio] = useState("");
+    const[location,setlocation] = useState("");
+
+
+    const bottomsheetref = useRef<BottomSheet>(null);
+    const snapPonints = useMemo(()=>["55%"],[]);
   //const [open, setOpen] = useState(false);
   const router = useRouter();
   return (
@@ -22,7 +33,7 @@ const profile = () => {
       <ScrollView>
         <TouchableOpacity
           style={styles.profilecontainer}
-          onPress={()=>router.push('/(main)/updateprofile')}    >
+          onPress={()=>bottomsheetref.current?.expand()}    >
           <Image
             source={require("../../assets/img/avatar.png")}
             style={styles.pfp}
@@ -35,6 +46,41 @@ const profile = () => {
           </View>
         </TouchableOpacity>
       </ScrollView>
+      <BottomSheet
+      ref={bottomsheetref}
+      index={-1}
+      snapPoints={snapPonints}
+      backgroundStyle={{backgroundColor:theme.colors.secondary}}
+      enablePanDownToClose={true}
+      >
+        <BottomSheetView style={styles.sheet} >
+            <Text style={styles.title}>Update Your Profile!!</Text>
+            <TextInput
+            placeholder='name'
+            placeholderTextColor={theme.colors.gray}
+            value={name}
+            onChangeText={setname}
+            style={styles.input}/>
+            <TextInput
+            placeholder='bio'
+            placeholderTextColor={theme.colors.gray}
+            value={bio}
+            onChangeText={setbio}
+            style={styles.input}/>
+            <TextInput
+            placeholder='address'
+            placeholderTextColor={theme.colors.gray}
+            value={location}
+            onChangeText={setlocation}
+            style={styles.input}/>
+            <TouchableOpacity style={styles.btn}>
+                <Text style={{color:"black", fontWeight:500}}>Profile pic</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.btn}>
+                <Text style={{color:"black", fontWeight:500}}>Save</Text>
+            </TouchableOpacity>
+        </BottomSheetView>
+      </BottomSheet>
     </ScreenWrapper>
   );
 };
@@ -74,4 +120,37 @@ const styles = StyleSheet.create({
   text: {
     fontWeight: 500,
   },
+  sheet:{
+    //backgroundColor:theme.colors.secondary,
+    justifyContent:"center",
+    flexDirection:"column",
+    alignItems:"center",
+    gap:10,
+    flex:1
+  },
+  title:{
+    fontWeight:500,
+    color:"#fff",
+    padding:10
+  },
+  input:{
+    width: "90%",
+    backgroundColor: "#414141",
+    paddingVertical: 18,
+    paddingHorizontal: 20,
+    borderRadius: 12,
+    marginBottom: 12,
+    color:"#fff"
+  },
+  btn:{
+    width: "90%",
+    backgroundColor: "#f1f1f1",
+    paddingVertical: 18,
+    paddingHorizontal: 20,
+    borderRadius: 12,
+    marginBottom: 12,
+    justifyContent:"center",
+    alignItems:"center",
+    
+  }
 });
